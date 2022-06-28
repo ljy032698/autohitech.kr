ed24d7374a27274e169086378fc7e9fe5ee47dab
Lee Jongyoung <leejongyoung0326@icloud.com>
2022-06-28 13:08:55 +0900

    Add atw series html

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('atc-sda/', views.atcsda),
+    path('atc-sdae/', views.atcsdae),
+    path('atw-01/', views.atw01),
+    path('atw-02/', views.atw02),
 ]
--- products/views.py
+++ products/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse("Hello Products")
     return render(request, 'products/atc-sda.html')
+
+def atcsda(request):
+    return render(request, 'products/atc-sda.html')
+
+def atcsdae(request):
+    return render(request, 'products/atc-sdae.html')
+
+def atw01(request):
+    return render(request, 'products/atw-01.html')
+
+def atw02(request):
+    return render(request, 'products/atw-02.html')
