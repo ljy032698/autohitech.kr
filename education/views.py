bea68ef34a4f17f73d0c5b9b1813b8d70dc66cd7
Lee Jongyoung <leejongyoung0326@icloud.com>
2022-06-28 14:56:19 +0900

    Initial jekyll proejct

--- education/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-
-def index(request):
-    return render(request, 'education/schedule.html')
--- introduce/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    # return HttpResponse("Hello Introduce")
-    return render(request, 'introduce/greeting.html')
-
-def history(request):
-    return render(request, 'introduce/history.html')
