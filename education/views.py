98b6154c86ff806357cb5fe569b207bc3cd3e7c7
Lee Jongyoung <leejongyoung0326@icloud.com>
2022-06-28 12:38:16 +0900

    Add education schedule

--- education/views.py
+++ education/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello Education")
+    return render(request, 'education/schedule.html')
