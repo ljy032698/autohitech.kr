b9d54762f2b144c9d80ca9b19d693f8d952570e6
Lee Jongyoung <leejongyoung0326@icloud.com>
2022-06-28 12:32:55 +0900

    Add board detail

--- downloads/views.py
+++ downloads/views.py
@@ -8,3 +8,11 @@ def index(request):
     board_list = Board.objects.order_by('-create_date')
     context = {'board_list': board_list}
     return render(request, 'downloads/board_list.html', context)
+
+def detail(request, board_id):
+    """
+    downloads 내용 출력
+    """
+    board = Board.objects.get(id=board_id)
+    context = {'board': board}
+    return render(request, 'downloads/board_detail.html', context)
