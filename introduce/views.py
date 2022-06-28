f3d1d8c42a0fdbbf0afa4191c36b5fac2014b582
Lee Jongyoung <leejongyoung0326@icloud.com>
2022-06-28 13:56:28 +0900

    Add history html

--- introduce/views.py
+++ introduce/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse("Hello Introduce")
     return render(request, 'introduce/greeting.html')
+
+def history(request):
+    return render(request, 'introduce/history.html')
