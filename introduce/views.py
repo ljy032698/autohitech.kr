a2c13cd70fe7de2b2b9fdb853b285f01572b7824
Lee Jongyoung <leejongyoung0326@icloud.com>
2022-06-28 12:28:13 +0900

    Add templates

--- introduce/views.py
+++ introduce/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello Introduce")
+    # return HttpResponse("Hello Introduce")
+    return render(request, 'introduce/greeting.html')
